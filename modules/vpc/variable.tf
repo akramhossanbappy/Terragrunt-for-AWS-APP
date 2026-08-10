dd335d77cd10a857105f9844b765a92cca057378
MD Akram Hossan Bappy <245820584+akram-hossan_port@users.noreply.github.com>
2026-08-10 07:01:54 +0100

    update VPC variable

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -16,26 +16,32 @@ variable "tier" {
 
 variable "public_subnets_cidr" {
   description = "List of public subnet cidr"
+  type = list(string)
 }
 
 variable "private_subnets_cidr" {
   description = "List of private subnets cidr"
+  type = list(string)
 }
 
 variable "secure_subnets_cidr" {
   description = "List of secure subnets cidr"
+  type = list(string)
 }
 
 variable "availability_zones_public" {
   description = "List of availability zones of public subnets"
+  type = list(string)
 }
 
 variable "availability_zones_private" {
   description = "List of availability zones of private subnets"
+  type = list(string)
 }
 
 variable "availability_zones_secure" {
-  description = "List of availability zones of private subnets"
+  description = "List of availability zones of secure subnets"
+  type = list(string)
 }
 
 variable "cidr_block-nat_gw" {
